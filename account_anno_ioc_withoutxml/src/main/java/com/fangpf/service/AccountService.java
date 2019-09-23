291dcd9754b600a362f52f9645e1353bd2a2a0e1
方鹏飞 <fangpf5@chinaunicom.cn>
2019-09-23 19:42:40 +0800

    注解IOC without xml use Configuration注解

---
+++ account_anno_ioc_withoutxml/src/main/java/com/fangpf/service/AccountService.java
@@ -0,0 +1,28 @@
+package com.fangpf.service;
+
+/**
+ * @author fangpf
+ * @version 1.0
+ * @date 2019-09-23 15:38
+ */
+
+import com.fangpf.domian.Account;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * 账户业务层接口
+ * */
+public interface AccountService {
+
+    List<Account> findAllAccount() throws SQLException;
+
+    Account findById(Integer id) throws SQLException;
+
+    void saveAccount(Account account) throws SQLException;
+
+    void updateAccount(Account account) throws SQLException;
+
+    void deleteAccount(Integer id) throws SQLException;
+}
---
+++ account_anno_ioc_withoutxml/src/main/java/config/SpringConfiguration.java
@@ -0,0 +1,9 @@
+package config;
+
+/**
+ * @author fangpf
+ * @version 1.0
+ * @date 2019-09-23 19:12
+ */
+public class SpringConfiguration {
+}
